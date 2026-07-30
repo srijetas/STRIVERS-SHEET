8ca90d401e86cf5895c7a936c3cd8143af1f1de0
Srijeta Sinha <srijetas@gmail.com>
2026-07-31 01:59:02 +0530

    pattern1.java

---
+++ PATTERN/pattern1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void pattern1(int n) {
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    
+        
+    }
