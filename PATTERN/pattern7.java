54d305ab4140873bafed34824a69ffe106ad6601
Srijeta Sinha <srijetas@gmail.com>
2026-08-01 01:17:40 +0530

    pattern7.java

---
+++ PATTERN/pattern7.java
@@ -0,0 +1,26 @@
+public class pattern {
+
+    public static void main() {
+
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <=n-row ; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <=2*row-1 ; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+ }
+
+OUTPUT:
+
+    *
+   ***
+  *****
+ *******
+*********
